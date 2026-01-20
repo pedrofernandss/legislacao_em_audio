a6839e7c6f9a22f6f0ae314bff05ce03d6c9a469
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-19 23:58:00 -0300

    ✨ feat: convert number to word in the iterative way

--- clean.py
+++ clean.py
@@ -1,7 +1,21 @@
 import re
 from num2words import num2words
 
+output = []
+final = []
 texto = "Eu tenho 12 laranjas"
+lista_texto = texto.split(" ")
+print(lista_texto)
+
+for palavra in lista_texto:
+    if palavra.isdigit():
+        output.append(num2words(palavra, lang='pt_BR'))
+    else:
+        output.append(palavra)
+
+final.append(' '.join(output))
+print(final)
+
 novo_texto = re.sub(r'\d+', lambda match: num2words(match.group(), lang='pt_BR'), texto)
 print(novo_texto)
 
